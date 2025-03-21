d35614339b7992e027425fae3cc0488bc729807b
Samgu Lee <cable8mm@gmail.com>
2025-03-21 19:52:38 +0900

    Testing auto collect (#6)
    * Testing auto collect
    
    * Modify `code-style.yml`
    
    * PHP Linting (Pint)
    
    * PHP Linting (Pint)
    
    ---------
    
    Co-authored-by: cable8mm <cable8mm@users.noreply.github.com>

--- src/Example.php
+++ src/Example.php
@@ -2,26 +2,23 @@
 
 namespace Cable8mm\Github;
 
-class Example {
+class Example
+{
     /**
      * Plus method
-     *
-     * @param int $x
-     * @param int $y
-     * @return int
      */
     public function plus(int $x, int $y): int
     {
-        return $x+$y;
+        return $x + $y;
     }
 
     public function minus(int $x, int $y): int
     {
-        return $x-$y;
+        return $x - $y;
     }
 
     public function times(int $x, int $y): int
     {
-        return $x*$y;
+        return $x * $y;
     }
 }
